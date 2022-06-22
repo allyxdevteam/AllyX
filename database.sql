32245eae1832491a43f2b83f00bdbf986b760e18
Meir Sedlis <meirsedlis@gmail.com>
2022-06-22 09:21:01 -0500

    add ally application and users to database.sql

--- database.sql
+++ database.sql
@@ -12,7 +12,8 @@ CREATE TABLE "user" (
 	"last_name" VARCHAR (255),
     "email" VARCHAR (255) NOT NULL,
     "phone_number" BIGINT NOT NULL,
-    "dob" DATE NOT NULL, 
+    "dob" DATE NOT NULL,
+    "city" VARCHAR (255), 
     "profile_pic" VARCHAR (1000),
     "verify_pic" VARCHAR (1000),
     "facebook_link" VARCHAR (1000),
@@ -81,4 +82,23 @@ CREATE TABLE "call-rating"(
 	"inserted_at" TIMESTAMPTZ DEFAULT now()
 );
 
--- ALLY APPLICATION TO BE ADDED ONCE WE RECEIVE THE INFORMATION FROM ALEX
+CREATE TABLE "ally-application"(
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" NOT NULL,
+	"inserted_at" TIMESTAMPTZ DEFAULT now(),
+	"updated_at" TIMESTAMPTZ DEFAULT now(),
+	"answer_1" VARCHAR (1000),
+	"answer_2" VARCHAR (1000),
+	"answer_3" VARCHAR (1000),
+	"answer_4" VARCHAR (1000),
+	"is_complete" BOOLEAN DEFAULT false,
+	"is_approved" BOOLEAN
+);
+
+INSERT INTO "public"."user"("id","username","password","first_name","last_name","email","phone_number","dob","profile_pic","verify_pic","facebook_link","twitter_link","instagram_link","average_stars","inserted_at","updated_at","is_ally","is_admin","is_reported","is_active","is_blocked","delete_requested")
+VALUES
+(1,E'member',E'$2a$10$Pmc81qgIsgodtZ5oewDgtOMO/VSMN86RpPu8wp6zItpkKk1M.Cq9a',NULL,NULL,E'member@heyallyx.com',2121234567,E'1989-01-21',NULL,NULL,NULL,NULL,NULL,NULL,E'2022-06-21 16:23:55.778121-05',E'2022-06-21 16:23:55.778121-05',FALSE,FALSE,FALSE,TRUE,FALSE,FALSE),
+(2,E'ally',E'$2a$10$uDFEOBXSpXse1vL8uWpEAuE5YPqJKmiENAg8n74ebkQdbWVV3GDjS',NULL,NULL,E'ally@heyallyx.com',9171234567,E'1991-06-21',NULL,NULL,NULL,NULL,NULL,NULL,E'2022-06-21 16:35:35.027964-05',E'2022-06-21 16:35:35.027964-05',FALSE,FALSE,FALSE,TRUE,FALSE,FALSE),
+(3,E'admin',E'$2a$10$MRth28l6HcDfakKNi6pZF.dRh63JnDK1.N9kA2hzeC8oo6UXIHk8W',NULL,NULL,E'admin@heyallyx.com',3471234567,E'1986-06-21',NULL,NULL,NULL,NULL,NULL,NULL,E'2022-06-21 16:36:53.483141-05',E'2022-06-21 16:36:53.483141-05',FALSE,FALSE,FALSE,TRUE,FALSE,FALSE);
+
+
