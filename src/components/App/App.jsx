02baa6f458bbd88dfee14bc3e833ec32732d99f0
Joe AB <joe.anthonybrown@gmail.com>
2022-06-22 12:24:04 -0500

    added a current dateTime variable to be sent to the db through a saga

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -91,7 +91,7 @@ function App() {
             {user.id ?
               // If the user is already logged in, 
               // redirect to the /user page
-              <Redirect to="/user" />
+              <Redirect to="/home" />
               :
               // Otherwise, show the login page
               <LoginPage />
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -12,66 +12,64 @@ import RegisterForm from '../RegisterForm/RegisterForm';
 import AllyApplication from '../AllyApplication/AllyApplication';
 
 function LandingPage() {
-  const [heading, setHeading] = useState('Welcome');
+
+  //variables that are react functions---------------------------------
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  //variables that evaluate to something----------------------------------
+  const user = useSelector((store) => store.user);
+  console.log('this is the user:', user);
+
+  const date = new Date();
+  const dateTime = date.toLocaleString();
+  console.log('this is the time', dateTime);
+
+  //local state---------------------------------------------------------
+  const [heading, setHeading] = useState('Welcome');
+
+
+  //other functions-----------------------------------------------------------
   const onLogin = (event) => {
     history.push('/login');
   };
 
-  const user = useSelector((store) => store.user);
-console.log('this is the user:',user);
+  function handleRequestCall() {
+    console.log('in handleRequestCall');
+    dispatch({
+      type: "POST_REQUESTED_CALL",
+      payload: dateTime
+  })
+
+  }
+
+  function handleScheduleCall() {
+    console.log('in handleScheduleCall');
+  }
 
   return (
     <div className="container">
       <h2>{heading}</h2>
 
-      <div className="grid">
-        <div className="grid-col grid-col_8">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
-          </p>
+      <button onClick={handleRequestCall}>Request a call</button>
 
-          <p>
-            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
-            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
-            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
-            finibus metus facilisis. Nullam eget lectus non urna rhoncus
-            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
-            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
-            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
-            congue.
-          </p>
+      <button onClick={handleScheduleCall}>Schedule a call</button>
 
-          <p>
-            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
-            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
-            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
-            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
-            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
-            amet nisi.
-          </p>
-        </div>
-        <div className="grid-col grid-col_4">
-          <RegisterForm />
 
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-        </div>
-        <AllyApplication />
+
+      <div className="grid-col grid-col_4">
+        <RegisterForm />
+
+        <center>
+          <h4>Already a Member?</h4>
+          <button className="btn btn_sizeSm" onClick={onLogin}>
+            Login
+          </button>
+        </center>
       </div>
+      <AllyApplication />
     </div>
+
   );
 }
 
