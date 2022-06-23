11824da11de5be0a7b6cc63cd70587d98f075c10
Meir Sedlis <meirsedlis@gmail.com>
2022-06-23 13:35:32 -0500

    write inputs for general comment

--- src/components/GeneralComment/GeneralComment.jsx
+++ src/components/GeneralComment/GeneralComment.jsx
@@ -1,17 +1,41 @@
-import { Typography, Rating } from "@mui/material";
+import { useState } from "react";
+
+import { Typography, Rating, TextField, Box } from "@mui/material";
+import React from "react";
 
 function GeneralComment() {
-  return;
-  <>
-    <Typography component="legend">Rating</Typography>
-    <Rating
-      name="simple-controlled"
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-    />
-  </>;
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+
+
+  const handleComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  return (
+
+    <Box sx={[{maxWidth: '35vw'},{m:'auto'}]}>
+      <Typography component="legend">Rating</Typography>
+      <Rating
+        type="rating"
+        name="general-rating"
+        value={rating}
+        onChange={(event, newRating) => {
+          setRating(newRating);
+        }}
+      />
+      <Typography component="legend">Comment</Typography>
+      <TextField 
+        type="comment"
+        name="general-comment"
+        fullWidth 
+        multiline 
+        maxRows={4} 
+        onChange={handleComment} 
+      />
+      </Box>
+   
+  );
 }
 
 export default GeneralComment;
