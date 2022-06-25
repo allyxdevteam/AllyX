4053f6abe20fc7cf954d493bf105e1d12dd957ad
Meir Sedlis <meirsedlis@gmail.com>
2022-06-25 13:18:03 -0500

    create users reducer and add to root

---
+++ src/redux/reducers/users.reducer.js
@@ -0,0 +1,14 @@
+const usersReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'SET_USERS':
+      return action.payload;
+    case 'UNSET_USERS':
+      return {};
+    default:
+      return state;
+  }
+};
+
+// user will be on the redux state at:
+// state.user
+export default usersReducer;
