7f23212ad2485dbcd143ef9de4a47ae31f53616d
Meir Sedlis <meirsedlis@gmail.com>
2022-06-26 08:47:46 -0500

    add useeffect to fetch users on the userreport component

--- src/components/AdminDashboard/Reports/UserReport.jsx
+++ src/components/AdminDashboard/Reports/UserReport.jsx
@@ -1,4 +1,17 @@
+import { useSelector, useDispatch} from 'react-redux';
+import { useEffect } from 'react';
+
 function UserReport(){
+
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        dispatch({type: 'FETCH_PROFILES'});
+    }, [])
+
+    const users = useSelector(store=>store.users);
+    console.log(users);
+    
     return(
         <h1>some users</h1>
     )
