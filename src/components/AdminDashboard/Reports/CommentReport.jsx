289d99124be0018635981064160b3329a7eb7951
Meir Sedlis <meirsedlis@gmail.com>
2022-06-27 14:12:45 -0500

    create CommentReport.jsx from UserReport

---
+++ src/components/AdminDashboard/Reports/CommentReport.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect} from "react";
+
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+
+function CommentReport() {
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch({ type: "FETCH_PROFILES" });
+    }, []);
+
+    const users = useSelector((store) => store.users);
+
+    return (
+        <Box sx={{ height: 600, width: "98%", margin: "auto" }}>
+          <DataGrid
+            rows={users}
+            columns={columns}
+            pageSize={10}
+            density="compact"
+            rowsPerPageOptions={[10]}
+            disableSelectionOnClick
+            components={{ Toolbar: GridToolbar }}
+            experimentalFeatures={{ newEditingApi: true }}
+            onCellEditStop={handleEdit}
+          />
+        </Box>
+      );
+    }
+    export default CommentReport;
