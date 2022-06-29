8269303e80342c377a2f40fba2159fb3574b27e2
Meir Sedlis <meirsedlis@gmail.com>
2022-06-29 09:49:01 -0500

    createGET route on report.router

--- server/routes/report.router.js
+++ server/routes/report.router.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
-});
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    if(req.user.is_admin){
+    const sqlText = `
+      SELECT * FROM "report"
+    `;
+    pool.query(sqlText)
+      .then((dbRes) => {
+        res.send(dbRes.rows);
+      })
+      .catch((dbErr) => {
+        console.log('error getting reports', dbErr);
+        res.sendStatus(500);
+      })}
+      else console.warn('403, admins only :)')
+    }
+  )
 
 /**
  * POST route template
