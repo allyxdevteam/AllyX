9a91e955b1d6f4373a1dcdd80db8b04128cb345a
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-06-29 16:48:58 -0500

    Created reportAbuse.saga

--- src/components/AllyReportAbuse/AllyReportAbuse.jsx
+++ src/components/AllyReportAbuse/AllyReportAbuse.jsx
@@ -1,19 +1,25 @@
+import {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 
 function ReportAbuse() {
 
+    const [abuseComments, setAbuseComments] = useState('');
     const dispatch = useDispatch();
     const claimedCallMember = useSelector(store => store.claimedCall.claimedCallMember);
 
     return (
         <>
+            <TextField 
+                onChange={() => {setAbuseComments(e.target.value)}}
+                value={abuseComments}
+            />
             <Button
                 onClick={() => {
                     dispatch({
                         type: 'ALLY_REPORT_ABUSE',
-                        payload: claimedCallMember
+                        payload: claimedCallMember.id
                     })
                 }}
             >
--- src/redux/sagas/reportAbuse.saga.js
+++ src/redux/sagas/reportAbuse.saga.js
@@ -1,3 +1,17 @@
+import axios from 'axios';
+import { takeLatest } from 'redux-saga/effects';
+
 function* allyReportAbuse(action) {
-    const memberId = action.payload.
+    const memberId = action.payload
+    yield axios({
+        method: 'PUT',
+        url: `/api/report`,
+        data: memberId
+    })
 }
+
+function* reportAbuseSaga() {
+    yield takeLatest('ALLY_REPORT_ABUSE', allyReportAbuse)
+}
+
+export default reportAbuseSaga;
