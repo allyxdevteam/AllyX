219cff2d6e61df1a30ad1929af670e9989b2e79f
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-06-29 22:10:19 -0500

    Created member reporting ally routes, needs work

--- server/routes/reportAbuse.router.js
+++ server/routes/reportAbuse.router.js
@@ -43,3 +43,44 @@ router.put('/ally', rejectUnauthenticated, (req, res) => {
     res.sendStatus(500);
   });
 })
+
+router.post('/member', rejectUnauthenticated, (req, res) => {
+  const sqlText = `
+  INSERT INTO "report" (reviewer_id, recipient_id, call_id, comment)
+    VALUES ($1, $2, $3, $4);
+  `;
+  const sqlValues = [
+      req.user.id,
+      req.body.,
+      req.body.,
+      req.body.abuseComments
+  ];
+  pool.query(sqlText, sqlValues)
+  .then((dbRes) => {
+  res.sendStatus(201);
+})
+.catch((dbErr) => {
+  console.log('INSERT database error', dbErr);
+  res.sendStatus(500);
+});
+})
+
+router.put('/member', rejectUnauthenticated, (req, res) => {
+const sqlText = `
+UPDATE "user"
+SET
+  is_reported=TRUE
+WHERE id = $1;
+`;
+sqlValues = [req.body.id];
+pool.query(sqlText, sqlValues)
+  .then((dbRes) => {
+  res.sendStatus(201);
+})
+.catch((dbErr) => {
+  console.log('INSERT database error', dbErr);
+  res.sendStatus(500);
+});
+})
+
+module.exports = router;
--- src/redux/sagas/reportAbuse.saga.js
+++ src/redux/sagas/reportAbuse.saga.js
@@ -17,9 +17,28 @@ function* markMemberReported(action) {
     })
 }
 
+function* memberReportAbuse(action) {
+    yield axios({
+        method: 'POST',
+        url: `/api/reportAbuse/member`,
+        data: action.payload
+    })
+}
+
+function* markAllyReported(action) {
+    yield axios({
+        method: 'PUT',
+        url: `/api/reportAbuse/member`,
+        data: action.payload.id
+    })
+}
+
+
 function* reportAbuseSaga() {
     yield takeLatest('ALLY_REPORT_ABUSE', allyReportAbuse);
     yield takeLatest('MARK_MEMBER_REPORTED', markMemberReported);
+    yield takeLatest('MEMBER_REPORT_ABUSE', memberReportAbuse);
+    yield takeLatest('MARK_ALLY_REPORTED', markAllyReported);
 }
 
 export default reportAbuseSaga;
