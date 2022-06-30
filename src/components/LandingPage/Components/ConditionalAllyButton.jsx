e8e9c850622581ce39a017fed94c71840b97c892
Meir Sedlis <meirsedlis@gmail.com>
2022-06-30 12:03:40 -0500

    add logic to ConditionallyAllyButton and import to LandingPage

---
+++ src/components/LandingPage/Components/ConditionalAllyButton.jsx
@@ -0,0 +1,28 @@
+import { useHistory } from "react-router-dom";
+
+import { Button } from "@mui/material";
+
+function ConditionalAllyButton(user) {
+  const history = useHistory();
+
+  function handleGoToAllyPage() {
+    history.push("/allyRequestedCalls");
+  }
+
+  function handleGoToAllyApplication() {
+    history.push("/ally-application");
+  }
+
+  if (user.is_ally) {
+    return <Button onClick={handleGoToAllyPage}>Go to Ally View</Button>;
+  }
+  return (
+    <Button
+      onClick={handleGoToAllyApplication}
+    >
+      Become an Ally
+    </Button>
+  );
+}
+
+export default ConditionalAllyButton;
