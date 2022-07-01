815f7001d09e57a591476f9ceeedc157fdaab750
Meir Sedlis <meirsedlis@gmail.com>
2022-07-01 14:54:48 -0500

    move RequestedCallItem to AllyRequestedCall/Components

--- src/components/AllyRequestedCalls/AllyRequestedCalls.jsx
+++ src/components/AllyRequestedCalls/AllyRequestedCalls.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import RequestedCallItem from '../RequestedCallItem/RequestedCallItem';
+import RequestedCallItem from './Components/RequestedCallItem';
 
 function AllyRequestedCalls(){
 
