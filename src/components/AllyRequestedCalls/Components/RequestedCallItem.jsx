f878d2063175dcea5a88cb32dc464aa85bf11df1
Meir Sedlis <meirsedlis@gmail.com>
2022-07-01 15:32:48 -0500

    add profile pictures to call requests

--- src/components/AllyRequestedCalls/Components/RequestedCallItem.jsx
+++ src/components/AllyRequestedCalls/Components/RequestedCallItem.jsx
@@ -1,47 +1,58 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import {ListItem, ListItemAvatar, ListItemText, Button, Typography, Avatar, Divider} from '@mui/material/';
+import {
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Button,
+  Typography,
+  Avatar,
+  Divider,
+} from "@mui/material/";
 
-function RequestedCallItem( {call} ){
+function RequestedCallItem({ call }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector((store) => store.user);
+  const memberId = call.member_id;
+  const requestedCallId = call.id;
+  const [picture, setPicture] = useState(null);
 
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const user = useSelector(store => store.user);
-    const memberId = call.member_id;
-    const requestedCallId = call.id;
+  useEffect(() => grabProfilePic, []);
 
-
-
-    function handleClaimCall(){
-        if(call.open===true){
-        dispatch({
-            type: 'POST_CLAIMED_CALL',
-            payload: {call, user}
-        })
-        dispatch({
-            type: 'FETCH_REQUESTED_CALLS',
-        })
-        history.push(`/allyStartCall/${memberId}/${requestedCallId}`)
-        }
-        else{
-            alert('this call has already been claimed')
-        }
+  function grabProfilePic() {
+    if (call.profile_pic) {
+      setPicture(call.profile_pic);
     }
+  }
 
-    
-    return(
-        <ListItem key={call.id}>
-            <ListItemAvatar>
-                <Avatar />
-            </ListItemAvatar>
-            <ListItemText primary={call.time} />
-        <Button onClick={handleClaimCall}>Claim Call</Button>
-        </ListItem>
-    )
-    
+  function handleClaimCall() {
+    if (call.open === true) {
+      dispatch({
+        type: "POST_CLAIMED_CALL",
+        payload: { call, user },
+      });
+      dispatch({
+        type: "FETCH_REQUESTED_CALLS",
+      });
+      history.push(`/allyStartCall/${memberId}/${requestedCallId}`);
+    } else {
+      alert("this call has already been claimed");
+    }
+  }
+
+  return (
+    <ListItem key={call.id}>
+      <ListItemAvatar>
+        <Avatar src={picture} />
+      </ListItemAvatar>
+      <ListItemText primary={call.time} />
+      <Button onClick={handleClaimCall}>Claim Call</Button>
+    </ListItem>
+  );
 }
 
-export default RequestedCallItem
+export default RequestedCallItem;
