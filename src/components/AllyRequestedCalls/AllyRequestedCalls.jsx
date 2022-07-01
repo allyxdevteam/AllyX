30460bb8b13437e8dd3ea13228667ba2ebca7e8f
Meir Sedlis <meirsedlis@gmail.com>
2022-07-01 17:02:00 -0500

    add pagination to requested call list

--- src/components/AllyRequestedCalls/AllyRequestedCalls.jsx
+++ src/components/AllyRequestedCalls/AllyRequestedCalls.jsx
@@ -1,39 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import RequestedCallItem from './Components/RequestedCallItem';
+import { List, Typography, Box, usePagination, Pagination } from "@mui/material";
 
-function AllyRequestedCalls(){
+import RequestedCallItem from "./Components/RequestedCallItem";
 
-    const dispatch = useDispatch();
+function AllyRequestedCalls() {
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        dispatch({
-            type: 'FETCH_REQUESTED_CALLS',
-        })
-    }, []);
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_REQUESTED_CALLS",
+    });
+  }, []);
 
-    const requestedCalls = useSelector(store => store.requestedCalls);
-    const user = useSelector(store => store.user);
+  const requestedCalls = useSelector((store) => store.requestedCalls);
+  const user = useSelector((store) => store.user);
 
+  //config pagination
+  const itemsPerPage = 7;
+  const [page, setPage] = useState(1);
+  const [noOfPages] = useState(Math.ceil(requestedCalls.length / itemsPerPage));
 
-    
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
-    return(
-        <ul>
-            {requestedCalls.map((call) => {
-                
-               return(
-               
-                <RequestedCallItem key={call.id} call={call} />
-                
-                )
-               
-            })}
-        </ul>
-    )
+  return (
+    <Box sx={{height: '80vh'}}>
+      <List>
+        {requestedCalls
+        .slice(page* itemsPerPage, page * itemsPerPage + itemsPerPage)
+        .map((call) => {
+          return <RequestedCallItem key={call.id} call={call} />;
+        })}
+      </List>
+      <Box display="flex" justifyContent="center">
+        <Pagination
+          count={noOfPages}
+          page={page}
+          onChange={handleChange}
+          defaultPage={1}
+          showFirstButton
+          showLastButton
+          autoResetPage="false"
+        />
+      </Box>
+      </Box>
+  );
 }
 
-
-export default AllyRequestedCalls
+export default AllyRequestedCalls;
