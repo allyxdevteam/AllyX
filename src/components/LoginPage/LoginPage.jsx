7c421580bd5ec1728817f217086c6eb080c35466
Meir Sedlis <meirsedlis@gmail.com>
2022-07-03 18:52:32 -0500

    add MUI style and remove redundant imports from LoginForm

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
+
+import {Typography} from '@mui/material';
 
 function LoginPage() {
   const history = useHistory();
