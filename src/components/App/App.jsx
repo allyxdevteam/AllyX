8119c96473dd94d36092c808cb8b097c17c00fe0
Meir Sedlis <meirsedlis@gmail.com>
2022-07-02 21:52:34 -0500

    add typography and boxDefault

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -50,7 +50,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        {/* <Nav /> */}
         <Switch>
           {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
           <Redirect exact from="/" to="/home" />
@@ -208,7 +208,7 @@ function App() {
           </Route>
         </Switch>        
         <BottomNav />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </Router>
   );
