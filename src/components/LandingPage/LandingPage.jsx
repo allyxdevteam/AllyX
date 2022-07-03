2c6d39d8bfd8005f00ea4cbca083b141ef19b467
Meir Sedlis <meirsedlis@gmail.com>
2022-07-02 21:26:45 -0500

    remove more redundant imports

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,33 +1,16 @@
 //react, redux, sagas------------------------------------------------------
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 //styling, mui ----------------------------------------------------------
 import './LandingPage.css';
 import { Box, Typography, Fade } from '@mui/material'
 
 
-// CUSTOM COMPONENTS---------------------------------------------------
-import ImageUploader from '../ImageUploader/ImageUploader';
-import ConditionalAllyButton from './Components/ConditionalAllyButton';
-
-
 function LandingPage() {
 
-  //variables that are react functions---------------------------------
-  const history = useHistory();
-  const dispatch = useDispatch();
-
   //variables that evaluate to something specific----------------------------------
-  const profileImage = useSelector((store) => store.profileImage)
   const user = useSelector((store) => store.user);
 
-
-  //local state---------------------------------------------------------
-
-  //other functions-----------------------------------------------------------
-  
 
   return (
     <Box className="boxDefault">
