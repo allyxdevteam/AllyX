97d03e4fe741b849e6099a442a6f91493cce9a91
Meir Sedlis <meirsedlis@gmail.com>
2022-07-03 10:20:21 -0500

    format inserted_at on call rating report

--- src/components/AdminDashboard/Reports/CallRatingReport.jsx
+++ src/components/AdminDashboard/Reports/CallRatingReport.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Swal from 'sweetalert2';
+import dayjs from 'dayjs';
 
 import {
   DataGrid,
@@ -57,6 +58,14 @@ function CallRatingReport() {
         headerName: "Date",
         width: 150,
         editable: false,
+        valueFormatter: (params) =>{
+          if(params.value == null) {
+            return('');
+          }
+  
+          const formattedDate = dayjs(params.value).format('MM/DD/YY, hh:mm A');
+          return `${formattedDate}`;
+        }
       },
       {
         field: "actions",
