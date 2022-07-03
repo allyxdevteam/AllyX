5127b21d02d7bdf7569109053980ce47b19c46d7
Meir Sedlis <meirsedlis@gmail.com>
2022-07-03 12:11:33 -0500

    apply MUI style, add profile pic ternary and icon for button

--- src/components/AllyCallInProgress/AllyCallInProgress.jsx
+++ src/components/AllyCallInProgress/AllyCallInProgress.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import {useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import {Button, Typography} from '@mui/material';
+import {Button, Typography, Box} from '@mui/material';
+import DoneIcon from '@mui/icons-material/Done';
 
 
 function AllyCallInProgress(){
@@ -23,9 +24,6 @@ function AllyCallInProgress(){
 
     const memberId = params.memberId
 
-    const date = new Date();
-    const dateTime = date.toLocaleString();
-
     function handleCompleteCall(){
         dispatch({
             type: 'PUT_CALL_ENDED_TIME_ALLY',
@@ -34,12 +32,37 @@ function AllyCallInProgress(){
         history.push('/allyReviewCall');
     }
 
-
     return(
-        <>
-        <Typography>Call in progress!</Typography>
-        <Button onClick={handleCompleteCall}>Call Complete</Button>
-        </>
+        <Box className="boxDefault">
+        <Typography variant="h2" align="center" sx={[{ mt: "5vh" }, {mb:2}]}>
+          You're talking to {claimedCallMember.first_name}
+        </Typography>
+        <Typography align="center" variant="h5" color="text.secondary" sx={{mb:2}}>
+            When you're finished, please remember to hit <i>call complete</i> and leave a rating and review of your call.
+        </Typography>
+        <Box sx={[{ width: "40vw" }, {maxWidth: '300px'}, { m: "auto" }, {mb:2}]}>
+          <img 
+            src={
+              claimedCallMember.profile_pic
+                ? claimedCallMember.profile_pic
+                : "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            }
+            alt="profile picture"
+          ></img>
+        </Box>
+  
+        <Box display="grid" justifyItems="center">
+          <Button
+            startIcon={<DoneIcon />}
+            variant="contained"
+            size="large"
+            onClick={handleCompleteCall}
+            sx={{ m: 2 }}
+          >
+            Call Complete
+          </Button>
+        </Box>
+      </Box>
     )
 }
 
