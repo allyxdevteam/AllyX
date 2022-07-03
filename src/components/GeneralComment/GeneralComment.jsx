7e640cd90799a5bbcc82ae584f8006a317bb2cdf
Meir Sedlis <meirsedlis@gmail.com>
2022-07-03 17:36:53 -0500

    style general comment to match review call components, add typography

--- src/components/GeneralComment/GeneralComment.jsx
+++ src/components/GeneralComment/GeneralComment.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 import { Typography, Rating, TextField, Box, Button } from "@mui/material";
 import React from "react";
@@ -10,45 +10,82 @@ function GeneralComment() {
 
   const dispatch = useDispatch();
 
-    // sets the comment variable with the content of the textfield
+  // sets the comment variable with the content of the textfield
   const handleComment = (e) => {
     setComment(e.target.value);
   };
 
   const handleSubmit = (e) => {
     dispatch({
-        type: 'ADD_GEN_COMMENT',
-        payload: { rating, comment }
-    })
+      type: "ADD_GEN_COMMENT",
+      payload: { rating, comment },
+    });
     setRating(0);
-    setComment('');
+    setComment("");
+    Swal.fire(
+      "Thank you! We take your feedback seriously. You are the best part of Allyx."
+    );
+    history.push("/");
   };
 
   return (
+    <Box className="boxDefault">
+      <Box sx={[{ pt: "5vh" }, { width: "80vw" }, { m: "auto" }]}>
+        <Typography align="center" variant="h2">
+          Use this form to give us feedback on your experience with Allyx, or
+          send us ideas for what you'd like to see from us in the future.
+        </Typography>
 
-    <Box sx={[{maxWidth: '35vw'},{m:'auto'}]}>
-      <Typography component="legend">Rating</Typography>
-      <Rating
-        type="rating"
-        name="general-rating"
-        value={rating}
-        onChange={(event, newRating) => {
-          setRating(newRating);
-        }}
-      />
-      <Typography component="legend">Comment</Typography>
-      <TextField 
-        type="comment"
-        name="general-comment"
-        value={comment}
-        fullWidth 
-        multiline 
-        maxRows={4} 
-        onChange={handleComment} 
-      />
-      <Button variant="contained" sx={{m:1}} onClick={handleSubmit}>Submit</Button>
+        <Typography align="center" variant="h4" color="text.secondary">
+          If you'd like to make a media inquiry or want to be sure you hear
+          back from us as soon as possible, send an email to hey @
+          heyallyx.com.
+        </Typography>
       </Box>
-   
+      <Box
+        sx={[
+          { m: "auto" },
+          { mt: 2 },
+          { maxWidth: "600px" },
+          { width: "80vw" },
+        ]}
+      >
+        <Typography variant="h5" component="legend">
+          Rating
+        </Typography>
+        <Rating
+          type="rating"
+          name="general-rating"
+          value={rating}
+          sx={{
+            fontSize: "2rem",
+          }}
+          onChange={(event, newRating) => {
+            setRating(newRating);
+          }}
+        />
+        <Typography variant="h5" component="legend">
+          Comment
+        </Typography>
+        <TextField
+          type="comment"
+          name="general-comment"
+          value={comment}
+          fullWidth
+          multiline
+          maxRows={4}
+          onChange={handleComment}
+        />
+        <Button
+          size="large"
+          variant="contained"
+          sx={{ m: 2 }}
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
   );
 }
 
