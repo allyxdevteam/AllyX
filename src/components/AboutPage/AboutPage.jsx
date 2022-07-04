9ad9d012ef1b9d29fa2c611a9b27d8cdf5b12b7f
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-07-04 14:16:18 -0500

    Added Team pictures, changed div to Box

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './AboutPage.css';
-import Typography from '@mui/material/Typography';
+import { Box, Typography } from '@mui/material';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -10,7 +10,74 @@ import Typography from '@mui/material/Typography';
 
 function AboutPage() {
   return (
-    <div className="container" id="about-page">
+    <Box className="boxDefault">
+      <Typography>
+        Team Allyx:
+      </Typography>
+      <Box
+        display="inline-block"
+      >
+        <Box>
+          <img
+            src="/images/Chris.jpg"
+            alt="Chris Boyd"
+            width="20%"
+          />
+          <Typography>
+            Chris
+          </Typography>
+          <Typography>
+            https://www.linkedin.com/in/chris-boyd-1695ba8/
+          </Typography>
+        </Box>
+        <Box
+          display="inline-block"
+        >
+          <img
+            src="/images/Ian.jpg"
+            alt="Ian Larsen"
+            width="20%"
+          />
+          <Typography>
+            Ian
+          </Typography>
+        </Box>
+        <Typography>
+          https://www.linkedin.com/in/ian-larsen-505824154/
+        </Typography>
+        <Box
+          display="inline-block"
+        >
+            <img
+            src="/images/Meir.jpg"
+            alt="Meir Sedlis"
+            width="112.195px"
+            height="74.805px"
+        />
+        <Typography>
+          Meir
+        </Typography>
+        <Typography>
+          https://www.linkedin.com/in/meirsedlis/
+        </Typography>
+        </Box>
+        <Box
+          display="inline-block"
+        >
+            <img
+              src="/images/Joe.jpeg"
+              alt="Joe Anthony-Brown"
+              width="112.195px"
+              height="74.805px"
+            />
+        <Typography>
+          Joe
+        </Typography>
+        <Typography>
+          https://www.linkedin.com/in/joe-anthony-brown/
+        </Typography>
+        </Box>
+    </Box>
       <div>
       <Typography
         variant="h4"
@@ -52,13 +119,13 @@ function AboutPage() {
         variant="subtitle2"
         gutterBottom
       >
-        CSS Bootstrap
+        MUI
       </Typography>
       <Typography
         variant="subtitle2"
         gutterBottom
       >
-        MUI
+        Cloudinary
       </Typography>
       </div>
       <Typography
@@ -97,7 +164,7 @@ function AboutPage() {
       >
         Friends and family
       </Typography>
-    </div>
+    </Box>
   );
 }
 
