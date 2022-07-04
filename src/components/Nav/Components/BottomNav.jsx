b020f3b5b61cd31a5d8487f8f85d57c86995c3f3
Meir Sedlis <meirsedlis@gmail.com>
2022-07-04 10:06:37 -0500

    get user data from the store, add labels

--- src/components/Nav/Components/BottomNav.jsx
+++ src/components/Nav/Components/BottomNav.jsx
@@ -1,47 +1,60 @@
-import { Logout, Phone, } from "@mui/icons-material";
-import  {
+import { Logout, Phone } from "@mui/icons-material";
+import {
   BottomNavigation,
   BottomNavigationAction,
   Avatar,
-  SpeedDial,
-  SpeedDialAction,
-} from "@mui/material"
+} from "@mui/material";
 
 import { Box } from "@mui/system";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import CallSpeedDial from "./CallSpeedDial";
 
 function bottomNav() {
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
 
-    const handleLogout = () => { dispatch({ type: 'LOGOUT' })}
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
-    const handleProfClick = ()=>{
-        history.push("/profile")
-    }
+  const handleProfileClick = () => {
+    history.push("/profile");
+  };
   return (
     <>
-      <BottomNavigation sx={[{ position: 'absolute', bottom: 0, left: 0, right: 0},{maxHeight:'20vh'},]}showLabels> 
-        <BottomNavigationAction label="" icon={<Logout />} onClick={handleLogout}/>
-
-         
-        <BottomNavigationAction label="" 
-        
-        
-        
-        //INSERT SPEED DIAL COMP HERE AS AN ICON
-        icon={<CallSpeedDial />}
+      <BottomNavigation
+        sx={[
+          { position: "absolute", bottom: 0, left: 0, right: 0 },
+          { maxHeight: "20vh" },
+        ]}
+        showLabels='true'
+      >
+    
+          <BottomNavigationAction
+            label="Logout"
+            icon={<Logout />}
+            onClick={handleLogout}
           />
-     
+
+
+        <BottomNavigationAction
+          label=""
+          //INSERT SPEED DIAL COMP HERE AS AN ICON
+          icon={<CallSpeedDial />}
+        />
 
         
-      
-   
-        <BottomNavigationAction label="" icon={<Avatar/>} onClick={handleProfClick}/>
+          <BottomNavigationAction
+            label=""
+            icon={<Avatar />}
+            onClick={handleProfileClick}
+          />
 
-       </BottomNavigation>
+          {}
+
+      </BottomNavigation>
     </>
   );
 }
