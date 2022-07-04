d6d669129e2fa3066ded4debfbee77f49c3fe9c3
Meir Sedlis <meirsedlis@gmail.com>
2022-07-04 09:36:05 -0500

    apply style from ally report misuse to member report misuse

--- src/components/MemberReportMisuse/MemberReportMisuse.jsx
+++ src/components/MemberReportMisuse/MemberReportMisuse.jsx
@@ -1,47 +1,58 @@
-import {useState} from 'react';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
 
 import { Button, Box, TextField, Typography } from "@mui/material";
 
 function MemberReportMisuse() {
+  const [misuseComments, setMisuseComments] = useState("");
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const requestedCallId = useSelector(
+    (store) => store.requestedCalls.requestedCall
+  );
+  const completedCall = useSelector(
+    (store) => store.claimedCall.oneCallReducerMember
+  );
+  const allyId = completedCall.ally_id;
 
-    const [misuseComments, setMisuseComments] = useState('');
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const requestedCallId = useSelector(store => store.requestedCalls.requestedCall);
-    const completedCall = useSelector(store => store.claimedCall.oneCallReducerMember);
-    const allyId = completedCall.ally_id;
-
-    return (
-        <Box
-            sx={[{height: '80vh'},{width: '98vw'}, {margin: 'auto'}]}
-            display='flex'
-            flexDirection='column' 
-            justifyContent = 'center'
-        >
-            <TextField 
-                onChange={(e) => {setMisuseComments(e.target.value)}}
-                value={misuseComments}
-            />
-            <Button
-                onClick={() => {
-                    dispatch({
-                        type: 'MARK_ALLY_REPORTED',
-                        payload: { allyId }
-                    })
-                    dispatch({
-                        type: 'MEMBER_REPORT_MISUSE',
-                        payload: { requestedCallId, allyId, misuseComments }
-                    })
-                    history.push('/');
-                }}
-            >
-                Report Misuse
-            </Button>
-        </Box>
-    )
+  return (
+    <Box display="flex" flexDirection="column" className="commentBox">
+      <Typography variant="h4" align="center" sx={{ m: 2 }}>
+        Please tell us what went wrong.
+      </Typography>
+      <TextField
+        sx={{ m: 2 }}
+        placeholder="describe what happened"
+        onChange={(e) => {
+          setMisuseComments(e.target.value);
+        }}
+        value={misuseComments}
+      />
+      <Button
+        sx={{ m: "auto" }}
+        color="warning"
+        variant="contained"
+        onClick={() => {
+          dispatch({
+            type: "MARK_ALLY_REPORTED",
+            payload: { allyId },
+          });
+          dispatch({
+            type: "MEMBER_REPORT_MISUSE",
+            payload: { requestedCallId, allyId, misuseComments },
+          });
+          Swal.fire(
+            "We have logged your report and will take action on it. If necessary, someone will follow up with you. Thank you for being a part of Allyx and taking the time to share your experience."
+          );
+          history.push("/");
+        }}
+      >
+        Report Problem
+      </Button>
+    </Box>
+  );
 }
 
 export default MemberReportMisuse;
