54a7d6df432f1481e9438b54d0dbd09ea0d3a1c1
Meir Sedlis <meirsedlis@gmail.com>
2022-07-04 12:43:06 -0500

    add links and routing to bottom nav

--- src/components/Nav/Components/BottomNav.jsx
+++ src/components/Nav/Components/BottomNav.jsx
@@ -1,11 +1,25 @@
-import { Logout, Phone } from "@mui/icons-material";
+import {
+  Logout,
+  Phone,
+  Assessment,
+  AccountCircle,
+  ContactPhone,
+  Info,
+  Home,
+  AssignmentTurnedIn,
+  Feedback,
+  MenuIcon
+} from "@mui/icons-material";
+
 import {
   BottomNavigation,
   BottomNavigationAction,
-  Avatar,
+  Badge,
+  Menu,
+  MenuItem
 } from "@mui/material";
 
-import { Box } from "@mui/system";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import CallSpeedDial from "./CallSpeedDial";
@@ -13,47 +27,98 @@ import CallSpeedDial from "./CallSpeedDial";
 function bottomNav() {
   const history = useHistory();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: "FETCH_REQUESTED_CALLS",
+    });
+  }, []);
+
   const user = useSelector((store) => store.user);
+  const requestedCalls = useSelector(
+    (store) => store.requestedCalls.requestedCalls
+  );
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
   };
 
-  const handleProfileClick = () => {
-    history.push("/profile");
-  };
   return (
     <>
       <BottomNavigation
+        fullWidth="true"
         sx={[
           { position: "absolute", bottom: 0, left: 0, right: 0 },
-          { maxHeight: "20vh" },
+          { maxHeight: "25vh" },
         ]}
-        showLabels='true'
+        showLabels="true"
       >
-    
-          <BottomNavigationAction
-            label="Logout"
-            icon={<Logout />}
-            onClick={handleLogout}
-          />
-
+        <BottomNavigationAction
+          label="Logout"
+          icon={<Logout />}
+          onClick={handleLogout}
+        />
 
         <BottomNavigationAction
+          label="Home"
+          icon={<Home />}
+          onClick={() => history.push("/home")}
+        />
+
+        <BottomNavigationAction
+          label="About"
+          icon={<Info />}
+          onClick={() => history.push("/about")}
+        />
+
+        <BottomNavigationAction
+          label="Feedback"
+          icon={<Feedback />}
+          onClick={() => history.push("/comment")}
+        />
+
+        <BottomNavigationAction
+          label="Profile"
+          icon={<AccountCircle />}
+          onClick={() => history.push("/profile")}
+        />
+
+        {user.is_admin && (
+          <BottomNavigationAction
+            label="Admin Dashboard"
+            icon={<Assessment />}
+            onClick={() => history.push("/admin")}
+          />
+        )}
+
+        {user.is_ally ? (
+          <BottomNavigationAction
+            label="Requested Calls"
+            icon={
+              <Badge
+                badgeContent={requestedCalls.length}
+                color="primary"
+                label="Requested Calls"
+              >
+                <ContactPhone />
+              </Badge>
+            }
+            onClick={() => history.push("/allyRequestedCalls")}
+          />
+        ) : (
+          <BottomNavigationAction
+            label="Apply to be an Ally"
+            icon={<AssignmentTurnedIn />}
+            onClick={console.log("yay")}
+          />
+        )}
+
+        <BottomNavigationAction
+          sx={[{ mr: 2 }, { ml: 2 }]}
           label=""
           //INSERT SPEED DIAL COMP HERE AS AN ICON
           icon={<CallSpeedDial />}
         />
-
-        
-          <BottomNavigationAction
-            label=""
-            icon={<Avatar />}
-            onClick={handleProfileClick}
-          />
-
-          {}
-
       </BottomNavigation>
     </>
   );
